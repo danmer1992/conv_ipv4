69d2e6169ad602a0c664e24b4568a7c00cd4b837
Danmer1992 <danmer2991@gmail.com>
2021-12-07 00:02:34 +0300

    ver_with_new_features

--- convert_ipv4_address.py
+++ convert_ipv4_address.py
@@ -1,7 +1,39 @@
-import tkinter as tk
 import ipaddress
+import tkinter as tk
 
-def conv_bin_sys():
+def del_value():
+    '''Удаление содержимого поля'''
+    return address_entry.delete(0, tk.END)
+
+def columnconf(index,size):
+    '''Функция размера колонок'''
+    return win.grid_columnconfigure(index, minsize=size)
+
+def rowconf(index,size):
+    '''Функция размера рядов'''
+    return win.grid_rowconfigure(index, minsize=size)
+
+def conv_hex_system():
+    '''Функция конвертации адреса в шестнадцатеричную систему'''
+    address = address_entry.get()
+    result = []
+    try:
+        address = str(ipaddress.ip_address(address))
+        func_ch_add = lambda x: x.split(sep='.')
+        temp = [hex(int(elem))[2:] for elem in func_ch_add(address)]
+        for elem in temp:
+            if len(elem) < 2:
+                while len(elem) != 2:
+                    elem = '0' + elem
+                result.append(elem)
+            elif len(elem) == 2:
+                result.append(elem)
+        address = '.'.join(result)
+        label_result['text']= f'Your address is \n{address}'
+    except ValueError:
+        label_result['text']='Введите корректный адрес'
+
+def conv_bin_system():
     '''Функция конвертации адреса в двоичную систему'''
     address = address_entry.get()
     result = []
@@ -21,40 +53,41 @@ def conv_bin_sys():
     except ValueError:
         label_result['text']='Введите корректный адрес'
 
-def conv_hex_system():
-    pass
-
-def del_value():
-    '''Удаление содержимого поля'''
-    return address_entry.delete(0, tk.END)
-
-def columnconf(index,size):
-    '''Функция размера колонок'''
-    return win.grid_columnconfigure(index, minsize = size)
-
-def rowconf(index,size):
-    '''Функция размера рядов'''
-    return win.grid_rowconfigure(index, minsize = size)
+def select_num_system():
+    num_system = int_value.get()
+    if num_system == 2:
+        conv_bin_system()
+    else:
+        conv_hex_system()
 
 win = tk.Tk()
 win.title('Converting IPv4 address')
 win.geometry('300x300')
 win.resizable(False, False)
 
+int_value = tk.IntVar()
+
 address_entry = tk.Entry(win, font=('Calibri', 15),bd = 4)
 address_entry.grid(column=0, row=0, columnspan=2, sticky='wens')
 
-convert_button = tk.Button(win, text='Convert your address', command=conv_bin_sys).grid(column=0, row=1, ipadx=5, ipady=5)
-delete_button = tk.Button(win, text='Delete content field', command=del_value).grid(column=1, row=1, ipadx=5, ipady=5)
+radiobutton_bin = tk.Radiobutton(win, text='to double system',variable=int_value, value=2, command=select_num_system, cursor="cross")
+radiobutton_bin.grid(column=0,row=1)
 
-label_result = tk.Label(win, font = ('Calibri',12))
-label_result.grid(column=0, row=2, columnspan=2, sticky='wens')
+radiobutton_hex = tk.Radiobutton(win, text='to hex system',variable=int_value, value=16, command=select_num_system, cursor="cross")
+radiobutton_hex.grid(column=1, row=1)
 
-columnconf(0,150)
-columnconf(1,150)
+delete_button = tk.Button(win, text='Delete content field', command=del_value)
+delete_button.grid(column=0, row=2, columnspan=2,ipadx=5, ipady=5)
 
-rowconf(0,50)
-rowconf(1,100)
-rowconf(2,150)
+label_result = tk.Label(win, font = ('Calibri',12),bg='gray')
+label_result.grid(column=0, row=3, columnspan=2, sticky='wens', pady=20)
+
+columnconf(0, 150)
+columnconf(1, 150)
+
+rowconf(0, 50)
+rowconf(1, 50)
+rowconf(2, 50)
+rowconf(3, 170)
 
 win.mainloop()
